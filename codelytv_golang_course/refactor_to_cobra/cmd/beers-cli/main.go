ade9cbf8eb76fea929ada899551eb6173b0be3a6
Manuel Rúa <imanurua@gmail.com>
2020-04-19 13:56:28 +0200

    Go to repository

--- codelytv_golang_course/refactor_to_cobra/cmd/beers-cli/main.go
+++ codelytv_golang_course/refactor_to_cobra/cmd/beers-cli/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"github.com/ManuRua/golab/codelytv_golang_course/refactor_to_cobra/internal/cli"
+	"github.com/ManuRua/golab/codelytv_golang_course/refactor_to_cobra/internal/storage/csv"
 	"github.com/spf13/cobra"
 )
 
 func main() {
+	csvRepo := csv.NewRepository()
+
 	rootCmd := &cobra.Command{Use: "beers-cli"}
-	rootCmd.AddCommand(cli.InitBeersCmd())
+	rootCmd.AddCommand(cli.InitBeersCmd(csvRepo))
 	rootCmd.AddCommand(cli.InitStoresCmd())
 	rootCmd.Execute()
 }
